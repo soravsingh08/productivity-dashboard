cf484bc3f38b4e3e1e52f262fb7957642975bd43
soravsingh08 <rahulrakwal3@gmail.com>
2025-12-19 12:37:25 +0530

    Day Planner

--- script.js
+++ script.js
@@ -18,54 +18,79 @@ function openFeatures() {
 openFeatures();
 
 //LOCAL STORAGE PE TASK RKHNA
+function todoList() {  
+  var currentTask = [];
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("Task list is empty");
+  }
 
-var currentTask = []
-if(localStorage.getItem('currentTask')){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'))
-}else{
-    console.log('Task list is empty')
-  
-}
-
-function renderTask() {
-  var allTask = document.querySelector(".allTask");
-  sum = "";
-  currentTask.forEach(function (elem) {
-    sum =
-      sum +
-      ` <div class="task">
+  function renderTask() {
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    let allTask = document.querySelector(".allTask");
+    let sum = "";
+    currentTask.forEach(function (elem, idx) {
+      sum =
+        sum +
+        ` <div class="task">
               <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-              <button>Mark as completed</button>
+              <button id = ${idx}>Mark as completed</button>
             </div>`;
+    });
+    allTask.innerHTML = sum;
+  }
+  renderTask();
+
+  let taskInput = document.querySelector(".addTask form input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let form = document.querySelector(".addTask form");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    //   console.log(taskInput.value);
+    //   console.log(taskDetailsInput.value);
+    //   console.log(taskCheckbox.checked)
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked,
+    });
+
+    renderTask();
+
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+    taskCheckbox.checked = false;
   });
-  allTask.innerHTML = sum;
+
+  document.querySelector(".allTask").addEventListener("click", function (e) {
+    if (e.target.tagName === "BUTTON") {
+      let index = e.target.id;
+      currentTask.splice(index, 1);
+      renderTask();
+    }
+  });
 }
-renderTask();
+todoList();
 
 
-let taskInput = document.querySelector(".addTask form input");
-let taskDetailsInput = document.querySelector(".addTask form textarea");
-let form = document.querySelector(".addTask form");
-let taskCheckbox = document.querySelector(".addTask form #check");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+/// Daily Planner
 
-  //   console.log(taskInput.value);
-  //   console.log(taskDetailsInput.value);
-  //   console.log(taskCheckbox.checked)
-  currentTask.push({
-    task: taskInput.value,
-    details: taskDetailsInput.value,
-    imp: taskCheckbox.checked,
-  });
+let hours = Array.from({length:18},function(elem,idx){
+  return `${6+idx}:00- ${7+idx}:00`
+})
 
-  localStorage.setItem('currentTask',JSON.stringify(currentTask))
-  taskInput.value =''
-  taskDetailsInput.value= ''
-  taskCheckbox.checked = 'false '
+let wholeDaySum ='';
+hours.forEach(function(elem){
+  wholeDaySum = wholeDaySum + ` <div class="day-planner-time">
+            <p>${elem}</p>
+            <input type="text" placeholder="...">
+          </div>`
+})
 
-
-renderTask();
-});
-
+var dayPlanner = document.querySelector('.day-planner')
+dayPlanner.innerHTML = wholeDaySum
